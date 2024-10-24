9b2218f1f8e69ccb0e0f597ca1d0508e965a2347
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2024-10-24 20:05:46 +0100

    fix dart logic

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 def check_dart(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if "Dart" not in request.headers:
+        if "Dart" not in request.headers['User-Agent']:
             return redirect('/')
         return f(*args, **kwargs)
     return decorated_function
@@ -14,6 +14,11 @@ def check_dart(f):
 @app.route('/')
 def hello_world():
     return render_template("home.html")
+
+@app.route('/dartcheck')
+def checker():
+    print(request.headers)
+    return {"data":200}
 
 @app.route('/api/<call>')
 @check_dart
@@ -27,7 +32,8 @@ def apicall(call):
 @app.route('/api/post/<task>')
 @check_dart
 def taskpost(task):
-    return {"data": task}
+    returnText = f"{task} has been created"
+    return {"data": returnText}
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
