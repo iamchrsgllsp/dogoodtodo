b385debdd5ed6a4b2c08acfad48b60609dd9c929
iamchrsgllsp <chrisgillespie2001@googlemail.com>
2024-10-24 18:41:49 +0100

    added api match

--- app.py
+++ app.py
@@ -3,7 +3,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Download our Android App'
+
+@app.route('/api/<call>')
+def apicall(call):
+    match call:
+        case "api":
+            return "hello"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
